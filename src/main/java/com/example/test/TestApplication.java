f754b4101b4f96b783a8cd2f6d45b46fe4eaa479
masiljangajji <hjk172262@gmail.com>
2024-02-13 11:30:29 +0900

    test

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	// test
+
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
